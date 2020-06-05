2a5c08e26cf9bcd8330c01d8ad83358831f53c09
ozdevi siri <ozdevi.siri@gmail.com>
2020-06-05 21:47:40 +0100

    Font loader brick

---
+++ src/bricks/FontLoader.vue
@@ -0,0 +1,34 @@
+<script>
+import MersenneTwister from '@dsibilly/mersenne-twister';
+import Brick from '@/Brick.vue';
+import format from 'date-fns/format';
+
+const rng = new MersenneTwister(format(new Date(), 'dMyyyy'));
+
+export default {
+  name: 'todays-lucy-number',
+  data() {
+    return { randomNumber: rng.randomInt() };
+  },
+  components: {
+    Brick,
+  },
+};
+</script>
+
+<template>
+  <brick name="todays-lucky-number">
+    <mark class="today-lucky-number">
+      <strong>
+        <em>Today's random number is {{randomNumber}}</em>
+      </strong>
+    </mark>
+  </brick>
+</template>
+
+<style scoped lang="scss">
+.today-lucky-number em{
+  text-decoration: none!important;
+  white-space: normal;
+}
+</style>
