1486dc5b4d7c09eb85b87c2daa163fcb98b47d53
ozdevi siri <ozdevi.siri@gmail.com>
2020-06-07 15:09:54 +0100

    Workers information

---
+++ src/bricks/WorkersActivity.vue
@@ -0,0 +1,25 @@
+<script>
+import Brick from '@/Brick.vue';
+
+export default {
+  name: 'orange',
+  components: {
+    Brick,
+  },
+};
+</script>
+
+<template>
+  <brick name="orange" tag="div">
+    <div class="orange brick-builder-orange">
+      <div>23 oranges for 23rd brick!</div>
+      <span>{{'🍊'.repeat(23)}}</span>
+    </div>
+  </brick>
+</template>
+
+<style scoped lang="scss">
+.orange {
+  margin-bottom: 16px;
+}
+</style>
