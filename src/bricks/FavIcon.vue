4109e8bfaacdb5700f23a39dd0ec897abec068db
ozdevi siri <ozdevi.siri@gmail.com>
2020-06-13 03:41:22 +0100

    Add a lil' brick favicon ( Thanks for the brick @zekehernandez )

---
+++ src/bricks/FavIcon.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+  name: 'fav-icon',
+  created() {
+    const favIconLink = document.getElementById('fav-icon');
+    favIconLink.href = 'favicon.ico';
+  },
+  destroyed() {
+    const favIconLink = document.getElementById('fav-icon');
+    favIconLink.href = 'data:;base64,iVBORw0KGgo='; // empty icon
+  },
+};
+</script>
+
+<template>
+<span></span>
+</template>
