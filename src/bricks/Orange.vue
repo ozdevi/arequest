0491ba53f89f789accd3e5e3c0b24b8fe241c849
ozdevi siri <ozdevi.siri@gmail.com>
2020-06-13 03:41:51 +0100

    Refactor orange brick

--- src/bricks/Orange.vue
+++ src/bricks/Orange.vue
@@ -1,21 +1,15 @@
 <script>
-import Brick from '@/Brick.vue';
 
 export default {
   name: 'orange',
-  components: {
-    Brick,
-  },
 };
 </script>
 
 <template>
-  <brick name="orange" tag="div">
-    <div class="orange brick-builder-orange">
-      <div>23 oranges for 23rd brick!</div>
-      <span>{{'🍊'.repeat(23)}}</span>
-    </div>
-  </brick>
+  <div class="orange brick-builder-orange">
+    <div>23 oranges for 23rd brick!</div>
+    <span>{{'🍊'.repeat(23)}}</span>
+  </div>
 </template>
 
 <style scoped lang="scss">
